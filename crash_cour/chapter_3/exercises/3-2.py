0c7133699ac05b2a1aa729c1b7773d6d6ce18764
eddboi <eddboi78@gmail.com>
2024-07-25 00:14:54 -0600

    cnttined to work on Python

---
+++ crash_cour/chapter_3/exercises/3-2.py
@@ -0,0 +1,8 @@
+# Greetings
+friends = ['Josh', 'Pablo', 'Keith', 'Seabass', 'Matt']
+message = f"\t My friend {friends[0].title()} likes to play video games.\n"
+message1 = f"\t My friend {friends[1].title()} likes to play video games.\n"
+message2 = f"\t My friend {friends[2].title()} likes to play video games.\n"
+message3 = f"\t My friend {friends[3].title()} likes to play video games.\n"
+message4 = f"\t My friend {friends[4].title()} likes to play video games."
+print(message,message1,message2,message3,message4)
---
+++ crash_cour/chapter_3/exercises/3-3.py
@@ -0,0 +1,7 @@
+# Your Own List
+trans = ['Car', 'Bike', 'Plane', 'Walk']
+message = f" I would like to own a Subaru {trans[0].title()}.\n"
+message1 = f"I would like to own a hybrid {trans[1].title()} to communte to work.\n"
+message2 = f"I like {trans[2].title()}s, but they are to expensie to own.\n"
+message3 = f"{trans[3].title()}ing is a great way to start the day."
+print(message,message1,message2,message3)
